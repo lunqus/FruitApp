89ed221494c2b0fc27cba28c0474ca727b137b60
LUNKUS <sigitas@lunkus.com>
2020-05-30 20:19:33 +0300

    CheckBox'es created

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,60 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Label title;
+    private Label responce;
+    private Label selected;
+
+    private CheckBox bananaCB;
+    private CheckBox mangoCB;
+    private CheckBox papayaCB;
+    private CheckBox grapefruitCB;
+    private String fruits;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.setTitle("Favorite Fruit");
+
+        title = new Label("What fruits do you like?");
+        responce = new Label("");
+        selected = new Label("");
+
+        bananaCB = new CheckBox("Banana");
+        mangoCB = new CheckBox("Mango");
+        papayaCB = new CheckBox("Papaya");
+        grapefruitCB = new CheckBox("Grapefruit");
+
+        // Setup the Stage and Scene
+        FlowPane rootPane = new FlowPane(Orientation.VERTICAL, 5, 5);
+        rootPane.setAlignment(Pos.CENTER);
+
+        // All of Controls
+        rootPane.getChildren().add(title);
+        rootPane.getChildren().addAll(bananaCB, mangoCB,
+                papayaCB, grapefruitCB, responce, selected);
+
+        Scene scene = new Scene(rootPane, 400,250);
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+        showAll();
+
+    }
+
+    void showAll() {
+        fruits = "";
+
     }
 
 
